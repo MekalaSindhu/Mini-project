fa86f3c2bdfe2b9bcc794136cf2f7ba859eb9c36
MekalaSindhu <mekalasaisindhu@gmail.com>
2025-09-24 19:12:59 +0530

    Create  iam.tf

---
+++ iam.tf
@@ -0,0 +1,58 @@
+# Create IAM role for EC2 instances to access S3
+resource "aws_iam_role" "ec2_s3_access_role" {
+  name = "${var.project_name}-ec2-s3-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-ec2-s3-role"
+  }
+}
+
+# IAM policy for S3 access
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "${var.project_name}-s3-access-policy"
+  description = "Policy for S3 bucket access"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::${var.s3_bucket_name}",
+          "arn:aws:s3:::${var.s3_bucket_name}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# Attach policy to role
+resource "aws_iam_role_policy_attachment" "ec2_s3_access" {
+  role       = aws_iam_role.ec2_s3_access_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+# Create instance profile
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "${var.project_name}-ec2-instance-profile"
+  role = aws_iam_role.ec2_s3_access_role.name
+}
