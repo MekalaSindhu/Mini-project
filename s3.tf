42c0c8c2d06806865699c84e71762464f13b45ba
MekalaSindhu <mekalasaisindhu@gmail.com>
2025-09-24 19:16:59 +0530

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,38 @@
+# Create private S3 bucket
+resource "aws_s3_bucket" "private_bucket" {
+  bucket = var.s3_bucket_name
+
+  tags = {
+    Name = "${var.project_name}-private-bucket"
+  }
+}
+
+# Configure S3 bucket privacy
+resource "aws_s3_bucket_public_access_block" "private_bucket" {
+  bucket = aws_s3_bucket.private_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+# S3 bucket versioning
+resource "aws_s3_bucket_versioning" "private_bucket" {
+  bucket = aws_s3_bucket.private_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# S3 bucket server-side encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "private_bucket" {
+  bucket = aws_s3_bucket.private_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
